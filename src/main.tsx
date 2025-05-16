351210ad7d8a411e473f553788a46041e896afaf
Alexey Korepanov <alexkorep@gmail.com>
2025-05-16 21:01:48 +0200

    Add PWA configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {
+    // e.g. notify your user that a new version is available
+    console.log('New content available, please refresh.')
+  },
+  onOfflineReady() {
+    // e.g. inform the user that app is ready to work offline
+    console.log('App is ready to work offline.')
+  }
+})
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-const repoName = "omega-void";
-
-// https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === "build" ? `/${repoName}/` : "/"; // Use repo name for build, root for dev
+export default defineConfig(() => {
+  const base = "/omega-void/";
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: ["favicon.svg", "robots.txt"],
+        manifest: {
+          name: "Omega Void",
+          short_name: "Omega Void",
+          description: "Omega Void",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              purpose: "maskable",
+              sizes: "1024x1024",
+              src: "icons/maskable_icon.png",
+              type: "image/png",
+            },
+            {
+              purpose: "maskable",
+              sizes: "192x192",
+              src: "icons/maskable_icon_x192.png",
+              type: "image/png",
+            },
+            {
+              purpose: "maskable",
+              sizes: "512x512",
+              src: "icons/maskable_icon_x512.png",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
     base: base, // Set the base path for deployment
     build: {
       outDir: "dist", // Ensure the output directory is 'dist' (matches workflow)
